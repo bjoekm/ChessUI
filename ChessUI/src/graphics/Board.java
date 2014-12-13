9c919c144855b0be86f5cb6c44b5d9391c7587fa
Björn Ekman <bjoek586@student.liu.se>
2014-12-13 01:39:53 +0100

    Possible to create a dummy frame to show a basic panel layout.

--- ChessUI/src/graphics/Board.java
+++ ChessUI/src/graphics/Board.java
@@ -1,12 +1,14 @@
 package graphics;
 
+import java.awt.Color;
+
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Board extends JPanel {
 
 	public Board(){
-		
+		this.setBackground(Color.CYAN);
 	}
 }
 
--- ChessUI/src/graphics/GameFrame.java
+++ ChessUI/src/graphics/GameFrame.java
@@ -1,17 +1,50 @@
 package graphics;
 
-import java.awt.HeadlessException;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+@SuppressWarnings("serial")
 public class GameFrame extends JFrame {
 
-	/**
-	 *  Class serializable becuase of JFrame. This number is auto generated per eclipse tips. 
-	 */
-	private static final long serialVersionUID = 1206308893586829811L;
-
+	public static final int DEFAULT_WIDTH = 800;
+	public static final int DEFAULT_HEIGHT = 600;
+	
 	public GameFrame()  {
-		// TODO Auto-generated constructor stub
+
+
+
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+		setLocationRelativeTo(null);
+		setTitle("R - Type");
+		setResizable(false);
+
+		setLayout(new BorderLayout());
+		JPanel north = new JPanel();
+		north.setPreferredSize(new Dimension(this.getWidth(), 20));
+		north.setBackground(Color.RED);
+		JPanel south = new JPanel();
+		south.setPreferredSize(new Dimension(this.getWidth(), 20));
+		south.setBackground(Color.GREEN);
+		JPanel east = new JPanel();
+		east.setPreferredSize(new Dimension(250, this.getHeight()));
+		east.setBackground(Color.BLUE);
+		add(north, BorderLayout.NORTH);
+		add(south, BorderLayout.SOUTH);
+		add(east, BorderLayout.EAST);
+		add(new Board(), BorderLayout.CENTER);
+		
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new GameFrame();
 	}
 }
+
+
