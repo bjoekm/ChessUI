0edb0d7fc7c571f3f7ecbbd851055698b8750bab
Björn Ekman <bjorn.ekman586@gmail.com>
2014-12-14 23:59:09 +0100

    Made sprite into a sublcass of rectangle

--- ChessUI/src/graphics/Sprite.java
+++ ChessUI/src/graphics/Sprite.java
@@ -3,23 +3,22 @@ package graphics;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Point;
+import java.awt.Rectangle;
 
-public class Sprite {
-
-	public static final int DEFAULT_SPRITE_POSITION = 5;
-	Image img;
-	Point pos;
+@SuppressWarnings("serial")
+public class Sprite extends Rectangle {
+	
+	private Image img;
 	
 	public Sprite(Image img){
 		this.img = img;
-		pos = new Point(DEFAULT_SPRITE_POSITION,DEFAULT_SPRITE_POSITION);
-	}
-	
-	public void setPos(Point newPos){
-		pos = newPos;
+		height = img.getHeight(null);
+		width = img.getWidth(null);
 	}
 	
 	public void draw(Graphics g){
-		g.drawImage(img, pos.x, pos.y, null);
+		int intx = Math.round((float)getX());
+		int inty = Math.round((float)getY());
+		g.drawImage(img, intx, inty, null);
 	}
 }
